772bf28cd0ffc929c164adb6ddeb0b0364679bd8
Github-0120 <68719780+Github-0120@users.noreply.github.com>
2020-07-29 14:46:17 +0900

    Add files via upload

---
+++ JS/basic.js
@@ -0,0 +1,11 @@
+alert("ご登録ありがとうございました！");
+
+function clickEvent(){
+    alert("早急にお支払いください");
+}
+
+function clickEvent2(){
+    alert("前ページはリンク切れです。"+"　お客様情報を入力してください");
+}
+
+console.log("submit");
